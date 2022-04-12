59a12025e0b05c469cc6fcaee09e3ba99e51f671
Phillip T Wittel <ptwittel@gmail.com>
2022-04-11 23:09:57 -0500

    Imputation added to master table

--- Data Clean/MasterTable script.R
+++ Data Clean/MasterTable script.R
@@ -1,11 +1,20 @@
 masterTable <- read_csv("Data Clean/master_table.csv")
 
-MT_gdpPop <- masterTable %>% 
-  mutate("Per capita GDP (in USD 2015)" = masterTable$`GPD (in USD 2015)`/ Population_2010)
+masterTable_simpName <- MT_gdpPop %>% 
+  rename(plastic_waste_gen = `Plastic waste generation (tonnes, total)`,
+         plastic_waste_per_cap = `Per capita plastic waste (kg/person/day)`,
+         mismanaged_waste = `Mismanaged waste (% global total)`,
+         percent_of_inadequate_managed = `Share of plastic inadequately managed`,
+         GDP = `GDP (in USD 2015)`)
 
-MT_gdpPop %>% filter(is.na(Population_2010) == 0) %>% 
-  pull(Entity) %>% 
-  str_detect("Island") %>% 
-  mean()
+MT_gdpPop <- masterTable_simpName %>% 
+  mutate(GDP_per_cap = masterTable$`GDP (in USD 2015)`/ Population_2010)
+
+
+MT_impute <- MT_gdpPop %>% select(-Code) %>% 
+                        mice::mice(., 
+                           m =5, 
+                           method = "rf",
+                           seed = 971)
 
 
