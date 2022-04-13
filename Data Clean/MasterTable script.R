39b6ddeda9f6353863d5ce871afbbb865d1c4455
Phillip T Wittel <ptwittel@gmail.com>
2022-04-12 19:55:08 -0500

    Added process for Imputation. this creates two CVS file in data clean folder

--- Data Clean/MasterTable script.R
+++ Data Clean/MasterTable script.R
@@ -1,20 +1,35 @@
-masterTable <- read_csv("Data Clean/master_table.csv")
+masterTable_raw <- read_csv("Data Clean/master_table.csv")
 
-masterTable_simpName <- MT_gdpPop %>% 
+
+masterTable <- masterTable_raw %>% 
   rename(plastic_waste_gen = `Plastic waste generation (tonnes, total)`,
          plastic_waste_per_cap = `Per capita plastic waste (kg/person/day)`,
          mismanaged_waste = `Mismanaged waste (% global total)`,
          percent_of_inadequate_managed = `Share of plastic inadequately managed`,
-         GDP = `GDP (in USD 2015)`)
+         GDP = `GDP (in USD 2015)`) 
 
-MT_gdpPop <- masterTable_simpName %>% 
-  mutate(GDP_per_cap = masterTable$`GDP (in USD 2015)`/ Population_2010)
+masterTable <- masterTable%>% 
+  mutate(GDP_per_cap = masterTable$`GDP`/ Population_2010)
 
 
-MT_impute <- MT_gdpPop %>% select(-Code) %>% 
+## Random Forest Imputation
+masterTable_rf <- masterTable %>% select(-Code) %>% 
                         mice::mice(., 
                            m =5, 
                            method = "rf",
                            seed = 971)
 
+masterTable_rf_complete <- complete(masterTable_rf)
 
+write_csv(masterTable_rf_complete, "Data Clean/MT_imp_rf.csv")
+
+## Cart Imputation
+masterTable_cart <- masterTable %>% select(-Code) %>% 
+  mice::mice(., 
+             m =5, 
+             method = "cart",
+             seed = 971)
+
+masterTable_cart_complete <- complete(masterTable_cart)
+
+write_csv(masterTable_cart_complete, "Data Clean/MT_imp_cart.csv")
