bd7c24c422745540d2db6c1427007add57b6f925
Phillip T Wittel <ptwittel@gmail.com>
2022-04-13 11:11:28 -0500

    Updated NA_table.R script to include life expentancy, etc. also reads and writes CSV files.

--- R scripts/NA_table.R
+++ R scripts/NA_table.R
@@ -1,20 +1,26 @@
+tab8 <- read_csv("Data Clean/plastic_1.csv")
+
 # NA EDA: Number of observations: 244
 NA_table <- data.frame(
-  "factor" = colnames(tab7%>% select(-Code, -Year, -Entity))[2:10],
+  "factor" = colnames(tab8%>% select(-Entity))[2:length(tab8)],
   
   sum_na = c(
-  sum(is.na(tab7$`Plastic waste generation (tonnes, total)`)), #Number of NAs: 76; proportion missing: 0.3114754
-  sum(is.na(tab7$Population)),                                 #Number of NAs: 17; proportion missing: 0.06967213
-  sum(is.na(tab7$`Annual Growth Rate %`)),                     #Number of NAs: 18; proportion missing: 0.07377049 #US missing 
-  sum(is.na(tab7$`Area (sq km)`)),                             #Number of NAs: 17; proportion missing: 0.06967213
-  sum(is.na(tab7$`Density (per sq km)`)),                      #Number of NAs: 17; proportion missing: 0.06967213
-  sum(is.na(tab7$`Per capita plastic waste (kg/person/day)`)), #Number of NAs: 58; proportion missing: 0.2377049
-  sum(is.na(tab7$`Mismanaged waste (% global total)`)),        #Number of NAs: 58; proportion missing: 0.2377049
-  sum(is.na(tab7$`Share of plastic inadequately managed`)),    #Number of NAs: 58; proportion missing: 0.2377049
-  sum(is.na(tab7$gdp_per_capita))                             #Number of NAs: 48; proportion missing: 0.1967213
+  sum(is.na(tab8$`Plastic waste generation (tonnes, total)`)), #Number of NAs: 76; proportion missing: 0.3114754
+  sum(is.na(tab8$Population)),                                 #Number of NAs: 17; proportion missing: 0.06967213
+  sum(is.na(tab8$`Annual Growth Rate %`)),                     #Number of NAs: 18; proportion missing: 0.07377049 #US missing 
+  sum(is.na(tab8$`Area (sq km)`)),                             #Number of NAs: 17; proportion missing: 0.06967213
+  sum(is.na(tab8$`Density (per sq km)`)),                      #Number of NAs: 17; proportion missing: 0.06967213
+  sum(is.na(tab8$`Total Fertility Rate`)),                     #Number of NAs: 18; proportion missing: 0.07377049
+  sum(is.na(tab8$`Life Expectancy at Birth`)),                 #Number of NAs: 18; proportion missing: 0.07377049
+  sum(is.na(tab8$`Under-5 Mortality Rate`)),                   #Number of NAs: 18; proportion missing: 0.07377049
+  sum(is.na(tab8$`Per capita plastic waste (kg/person/day)`)), #Number of NAs: 58; proportion missing: 0.2377049
+  sum(is.na(tab8$`Mismanaged waste (% global total)`)),        #Number of NAs: 58; proportion missing: 0.2377049
+  sum(is.na(tab8$`Share of plastic inadequately managed`)),    #Number of NAs: 58; proportion missing: 0.2377049
+  sum(is.na(tab8$gdp_per_capita))                             #Number of NAs: 48; proportion missing: 0.1967213
    )
 )
 
 NA_table <- NA_table %>% 
-  mutate(prop = sum_na / nrow(tab7))
+  mutate(prop = sum_na / nrow(tab8))
 
+write_csv(NA_table, "Data Clean/NA_table.csv")
